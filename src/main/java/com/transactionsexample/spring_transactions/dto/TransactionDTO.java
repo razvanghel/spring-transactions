e2436b70b63757a09e0b8987911aa7062e98d2d7
razanghel <aanghelrazvan99@gmail.com>
2024-07-05 19:21:15 +0200

    Add entity and DTO classes

---
+++ src/main/java/com/transactionsexample/spring_transactions/dto/TransactionDTO.java
@@ -0,0 +1,43 @@
+package com.transactionsexample.spring_transactions.dto;
+
+import java.time.LocalDateTime;
+
+public class TransactionDTO {
+
+    private Long id;
+    private Double amount;
+    private Long accountId;
+    private LocalDateTime timestamp;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public Long getAccountId() {
+        return accountId;
+    }
+
+    public void setAccountId(Long accountId) {
+        this.accountId = accountId;
+    }
+
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(LocalDateTime timestamp) {
+        this.timestamp = timestamp;
+    }
+}
