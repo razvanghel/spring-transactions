f6f9a931511d7252fdffb9762e1ee787a439569d
razanghel <aanghelrazvan99@gmail.com>
2024-07-05 19:22:26 +0200

    edit main class & remove empty test

--- src/main/java/com/transactionsexample/spring_transactions/SpringTransactionsApplication.java
+++ src/main/java/com/transactionsexample/spring_transactions/SpringTransactionsApplication.java
@@ -1,9 +1,12 @@
 package com.transactionsexample.spring_transactions;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 public class SpringTransactionsApplication {
 
 	public static void main(String[] args) {
