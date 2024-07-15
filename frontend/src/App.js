6b5c5b5ec6ac0383554a125eea1a1231c78dbde0
razanghel <aanghelrazvan99@gmail.com>
2024-07-15 21:44:25 +0200

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import CreateUserForm from './components/forms/CreateUserForm';
+import UserList from './components/UserList';
+import CreateAccountForm from './components/forms/CreateAccountForm';
+import CreateTransactionForm from './components/forms/CreateTransactionForm';
+const App = () => {
+    const [refreshUsers, setRefreshUsers] = useState(false);
+    const [refreshAccounts, setRefreshAccounts] = useState(false);
+    const [refreshTransactions, setRefreshTransactions] = useState(false);
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    const handleUserCreated = () => {
+        alert('User created successfully');
+        setRefreshUsers(!refreshUsers); // Toggle the refresh state to trigger re-fetching the user list
+    };
+
+    const handleAccountCreated = () => {
+        alert('Account created successfully');
+        setRefreshUsers(!refreshUsers);
+        setRefreshAccounts(!refreshAccounts); // Toggle the refresh state to trigger re-fetching the account list
+    };
+
+    const handleTransactionCreated = () => {
+        alert('Transaction created successfully');
+        setRefreshTransactions(!refreshTransactions); // Toggle the refresh state to trigger re-fetching the transactions list
+    };
+
+    return (
+        <div>
+            <h1>Current Account Management</h1>
+            <CreateUserForm onUserCreated={handleUserCreated} />
+            <p></p>
+            <CreateAccountForm onAccountCreated={handleAccountCreated} />
+            <p></p>
+            <CreateTransactionForm onTransactionCreated={handleTransactionCreated} />
+            <p></p>
+
+             <UserList key={refreshUsers} />
+            <hr />
+        </div>
+    );
+};
 
 export default App;
