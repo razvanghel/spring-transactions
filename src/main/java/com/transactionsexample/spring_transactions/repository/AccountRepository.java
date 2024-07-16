1170af5105edc7d0153b025e5ca81407768bbcab
razanghel <aanghelrazvan99@gmail.com>
2024-07-16 03:13:02 +0200

    refactor AccountResponseDTO into AccountDTO for account Repository

--- src/main/java/com/transactionsexample/spring_transactions/repository/AccountRepository.java
+++ src/main/java/com/transactionsexample/spring_transactions/repository/AccountRepository.java
@@ -1,10 +1,6 @@
 package com.transactionsexample.spring_transactions.repository;
 
-import com.transactionsexample.spring_transactions.dto.AccountRequestDTO;
-import com.transactionsexample.spring_transactions.dto.AccountResponseDTO;
-import com.transactionsexample.spring_transactions.model.Account;
-import com.transactionsexample.spring_transactions.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
+import com.transactionsexample.spring_transactions.dto.AccountDTO;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,13 +8,16 @@ import java.util.List;
 @Repository
 public interface AccountRepository {
 
-    void save(AccountRequestDTO account);
+    AccountDTO save(AccountDTO account);
 
-    AccountResponseDTO findById(Long id);
+    AccountDTO findById(Long id);
 
-    List<AccountResponseDTO> findByCustomerId(Long customerId);
+    List<AccountDTO> findByCustomerId(Long customerId);
 
-    List<AccountResponseDTO> findAll();
+    List<AccountDTO> findAll();
 
     void deleteById(Long id);
+
+    boolean existsById(Long id);
+
 }
